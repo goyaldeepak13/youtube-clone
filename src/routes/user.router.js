b29fad8e5316f11615c382c83c76e08c7618f111
goyaldeepak13 <deepakgoyalbari@gmail.com>
2024-03-09 23:35:36 +0530

    Add new route for userController

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateCoverImage, updateUserAvatar } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -19,8 +19,16 @@ router.route("/register").post( // The route is configured to accept HTTP POST r
 ) // now when we come to /register then registerUser will call which is in userController
 
 router.route("/login").post(loginUser)
-router.route("/refresh-token").post(refreshAccessToken)
+
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser) // first this verifyJWT middleware will run and in verifyJWT we called next() it means after verifyJWT middleware logoutUser will call
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser) // basically here we are not sending data so we can write get and user should be logged in so we have to write verifyJWT
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)// basically if we write post then all details will be update
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar) // basically middleware verifyJWT is for check that user is login or not and second middleware upload is for multer
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImgae"), updateCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) // basically we are taking route name from params so there must be colon and we can write /channel also
+router.route("/history").get(verifyJWT, getWatchHistory)
 export default router 
